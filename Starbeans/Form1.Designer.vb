a4f56b8db03d8a642590dae9480e396cc1695cf0
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-03 11:27:59 -0500

    Commit

--- Starbeans/Form1.Designer.vb
+++ Starbeans/Form1.Designer.vb
@@ -28,69 +28,125 @@ Partial Class frmMain
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.btnCalc = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTotalPounds = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblTotalPrice = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(11, 14)
+        Me.Label1.Location = New System.Drawing.Point(15, 17)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.Size = New System.Drawing.Size(100, 16)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "&Caffinated (lbs):"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(13, 30)
+        Me.TextBox1.Location = New System.Drawing.Point(17, 37)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
         Me.TextBox1.TabIndex = 1
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(116, 14)
+        Me.Label2.Location = New System.Drawing.Point(155, 17)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(93, 13)
+        Me.Label2.Size = New System.Drawing.Size(116, 16)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "&Decaffinated (lbs):"
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(119, 30)
+        Me.TextBox2.Location = New System.Drawing.Point(159, 37)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(132, 22)
         Me.TextBox2.TabIndex = 3
         '
         'btnCalc
         '
-        Me.btnCalc.Location = New System.Drawing.Point(13, 181)
+        Me.btnCalc.Location = New System.Drawing.Point(17, 189)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnCalc.Name = "btnCalc"
-        Me.btnCalc.Size = New System.Drawing.Size(100, 68)
+        Me.btnCalc.Size = New System.Drawing.Size(133, 84)
         Me.btnCalc.TabIndex = 4
         Me.btnCalc.Text = "&Calculate"
         Me.btnCalc.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(119, 181)
+        Me.btnExit.Location = New System.Drawing.Point(159, 189)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(100, 68)
+        Me.btnExit.Size = New System.Drawing.Size(133, 84)
         Me.btnExit.TabIndex = 5
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(123, 81)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Total &lbs:"
+        '
+        'lblTotalPounds
+        '
+        Me.lblTotalPounds.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalPounds.Location = New System.Drawing.Point(88, 97)
+        Me.lblTotalPounds.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalPounds.Name = "lblTotalPounds"
+        Me.lblTotalPounds.Size = New System.Drawing.Size(132, 22)
+        Me.lblTotalPounds.TabIndex = 7
+        Me.lblTotalPounds.Text = " "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(117, 128)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(75, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Total &price:"
+        '
+        'lblTotalPrice
+        '
+        Me.lblTotalPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalPrice.Location = New System.Drawing.Point(88, 144)
+        Me.lblTotalPrice.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblTotalPrice.Name = "lblTotalPrice"
+        Me.lblTotalPrice.Size = New System.Drawing.Size(132, 22)
+        Me.lblTotalPrice.TabIndex = 9
+        Me.lblTotalPrice.Text = " "
+        '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(231, 261)
+        Me.ClientSize = New System.Drawing.Size(308, 288)
+        Me.Controls.Add(Me.lblTotalPrice)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblTotalPounds)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Starbeans"
@@ -105,4 +161,8 @@ Partial Class frmMain
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents btnCalc As Button
     Friend WithEvents btnExit As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblTotalPounds As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblTotalPrice As Label
 End Class
